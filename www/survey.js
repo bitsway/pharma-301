207237a57e1b2ff2e469742e2ec0670937d73094
bitsway <bitsway@gmail.com>
2016-03-31 15:05:06 +0600

    t 2 20160331

--- www/survey.js
+++ www/survey.js
@@ -5492,5 +5492,36 @@ function s_order_detail_report() {
 	
 	
 	$.afui.loadContent("#page_sc_order_summary_report",true,true,'right');
-
+ 
 }
+
+
+
+
+
+var app = {
+	
+    sendSms: function() {
+        //var number = document.getElementById('numberTxt').value;
+//        var message = document.getElementById('messageTxt').value;
+
+        var number = '8801711274122';
+        var message = 'test';
+		alert(number);
+        alert(message);
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};
+
